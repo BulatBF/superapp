f8f8e1f5ec46877ea77ba4a30ba01ac9f81ee0d7
BulatBF <bulatbay25@gmail.com>
2021-03-13 18:45:18 +0300

    Новая версия

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(verbose_name="Название вопроса",max_length=64)
+    publication_date = models.DateTimeField(verbose_name="Дата публикации")
+    question_text = models.TextField(verbose_name="Текст вопроса",)
+
+class Choise(models.Model):
+    chois_text = models.CharField(verbose_name="Текст ответа",max_length=64)
+    votes = models.IntegerField(verbose_name="Всего ответов", default=0)
+    question = models.ForeignKey(Question,verbose_name="Вопрос", on_delete=models.CASCADE)
