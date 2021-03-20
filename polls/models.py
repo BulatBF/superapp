80e8e0567ce62f0a9da5984a5d51cded7a3c2786
BulatBF <bulatbay25@gmail.com>
2021-03-20 18:41:38 +0300

    Обновил сайт

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,12 @@ class Question(models.Model):
     title = models.CharField(verbose_name="Название вопроса",max_length=64)
     publication_date = models.DateTimeField(verbose_name="Дата публикации")
     question_text = models.TextField(verbose_name="Текст вопроса",)
+    def __str__(self) -> str:
+        return self.title
 
 class Choise(models.Model):
     chois_text = models.CharField(verbose_name="Текст ответа",max_length=64)
     votes = models.IntegerField(verbose_name="Всего ответов", default=0)
     question = models.ForeignKey(Question,verbose_name="Вопрос", on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.chois_text
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,28 @@
+from django import template
+from polls.models import Question
+from django.db.models.query_utils import Q
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response, Http404
+from django.template import context, loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello? Are you here? Is here here?")
+    all_questions = Question.objects.order_by("-publication_date")
+    template = loader.get_template("polls/index.html")
+    context = {"all_questions": all_questions}
+    return render(request, "polls/index.html", context)
+
+
+def results(request, question_id):
+    response = f"Список ответов на вопрос {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    response = f"Ваш ответ на {question_id}"
+    return HttpResponse(response)
+
+
+def detail(request, question_id):
+    response = f"Вы читаете вопрос {question_id}"
+    return HttpResponse(response)
